5ff6fd1447449169408a6153d043f6b99c28c40e
+ <kibetg984@gmail.com>
2024-11-08 16:02:31 +0300

    Updated the hooks simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetching a random dog image from the API
+    const fetchDogImage = async () => {
+      setLoading(true);
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await response.json();
+      setDogImage(data.message);
+      setLoading(false);
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+};
+
+export default App;
